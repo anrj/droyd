57bfe58f6f41c3618958ecc8a568091e4da9b11f
Anri Javakhishvili <Javakhishvili.Anri@kiu.edu.ge>
2025-02-25 22:37:18 +0400

    fix: (hopefully) discord embed images disappearing if set with url

--- commands/rasadrodis/rasadrodis.js
+++ commands/rasadrodis/rasadrodis.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder, InteractionContextType, ApplicationIntegrationType, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder, InteractionContextType, ApplicationIntegrationType, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
 const { scrapeMoazrovne } = require('../../utils/rasadrodis.js');
+const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,7 +10,7 @@ module.exports = {
 		.setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall]),
 	async execute(interaction) {
 		await interaction.deferReply();
-		const { questionNumber, question, questionImage, answer, explanation } = await scrapeMoazrovne();
+		const { questionNumber, question, questionImageUrl, answer, explanation } = await scrapeMoazrovne();
 
 		const revealButton = new ButtonBuilder()
 			.setCustomId('reveal')
@@ -22,17 +23,29 @@ module.exports = {
 		const questionEmbed = new EmbedBuilder()
 			.setTitle(`შეკითხვა #${questionNumber}`)
 			.addFields({ name: '', value: question, inline: true })
-			.setImage(questionImage)
 			.setColor('#93090a');
+
+		const files = [];
+		if (questionImageUrl) {
+			const filename = path.basename(questionImageUrl);
+			const imagePath = path.join(__dirname, '..', '..', 'utils', 'media', 'outputs', 'rasadrodis', filename);
+			const image = new AttachmentBuilder(imagePath);
+			files.push(image);
+			questionEmbed.setImage(`attachment://${filename}`);
+		}
 
 		const answerEmbed = new EmbedBuilder()
 			.addFields({ name: 'პასუხი:', value: `||${answer}||`, inline: false })
-			.addFields({ name: 'კომენტარი:', value: `||${explanation ?? 'არ გააჩნია'}||`, inline: false })
 			.setColor('#93090a');
+
+		if (explanation) {
+			answerEmbed.addFields({ name: 'კომენტარი:', value: `||${explanation}||`, inline: false });
+		}
 
 		const questionResponse = await interaction.editReply({
 			embeds: [questionEmbed],
 			components: [buttonRow],
+			files: files,
 			withResponse: true,
 		});
 
@@ -49,6 +62,7 @@ module.exports = {
 			await interaction.editReply({
 				embeds: [questionEmbed],
 				components: [buttonRow],
+				files: files,
 			});
 		});
 	},
--- utils/rasadrodis.js
+++ utils/rasadrodis.js
@@ -1,4 +1,7 @@
 const cheerio = require('cheerio');
+const fs = require('fs/promises');
+const axios = require('axios');
+const path = require('path');
 
 function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,7 +15,13 @@ async function scrapeMoazrovne() {
 	const $ = cheerio.load(await response.text());
 
 	const question = $('.question_question').clone().children('img').remove().end().text().trim();
-	const questionImage = $('.question_question img').attr('src') || null;
+	const questionImageUrl = $('.question_question img').attr('src') || null;
+	if (questionImageUrl) {
+		const imageResponse = await axios.get(questionImageUrl, { responseType: 'arraybuffer' });
+		const imageName = path.basename(questionImageUrl);
+		const imagePath = path.join(__dirname, 'media', 'outputs', 'rasadrodis', imageName);
+		await fs.writeFile(imagePath, Buffer.from(imageResponse.data));
+	}
 	const answer = $('.answer_body .clearfix').first().find('.right_nofloat').text().trim();
 	const isExplanation = $('.answer_body .clearfix').eq(1).find('.left').text().trim();
 	let explanation = $('.answer_body .clearfix').eq(1).find('.right_nofloat').text().trim();
@@ -24,12 +33,11 @@ async function scrapeMoazrovne() {
 	return {
 		questionNumber,
 		question,
-		questionImage,
+		questionImageUrl,
 		answer,
 		explanation,
 	};
 }
-
 module.exports = {
 	scrapeMoazrovne,
 };
