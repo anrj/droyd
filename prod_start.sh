a13a0d70a741d1d94e7d6aa60107b3faa5421529
Anri Javakhishvili <Javakhishvili.Anri@kiu.edu.ge>
2025-12-07 18:28:44 +0400

    fix: fixed previous commit bug

--- prod_start.sh
+++ prod_start.sh
@@ -2,19 +2,17 @@
 
 echo "Starting production deployment..."
 
-# Replace dev_ with prod_ in deploy-commands.js
 echo "Updating deploy-commands.js to use production credentials..."
-sed -i 's/dev_token/prod_token/g' deploy-commands.js
-sed -i 's/dev_clientId/prod_clientId/g' deploy-commands.js
+sed -i 's/\.setToken(dev_token)/.setToken(prod_token)/' deploy-commands.js
+sed -i 's/applicationCommands(dev_clientId)/applicationCommands(prod_clientId)/' deploy-commands.js
 
-# Replace dev_ with prod_ in index.js
 echo "Updating index.js to use production credentials..."
-sed -i 's/dev_token/prod_token/g' index.js
+sed -i 's/\.login(dev_token)/.login(prod_token)/' index.js
 
 echo "Deploying slash commands to Discord..."
 node deploy-commands.js
 
-sleep 5
+sleep 3
 
 echo "Starting the bot..."
 node index
